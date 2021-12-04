064d994ee29525737f74f70250e01102efdde472
Garrett Griffin <griffin.garrett@gmail.com>
2021-12-03 21:33:35 -0600

    chore: undo links added to body description

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -3,16 +3,13 @@ import { InfoTile } from "./InfoTile";
 const detailsContent =
   "Dump bed trailers for self load. Large object removal service.";
 
-const pricingContent = `Pricing available by weight or volume. Daily or weekly rates are available. Call ${(
-  <a href='tel:+1-913-353-5802'>(913)-353-5802</a>
-)} or send us an email at ${(
-  <a href='mailto:louisburgjunk@gmail.com'>louisburgjunk@gmail.com</a>
-)} for more pricing details.`;
+const pricingContent =
+  "Pricing available by weight or volume. Daily or weekly rates are available. Call (913)-353-5802 or send us an email at louisburgjunk@gmail.com for more pricing details.";
 
 export const Body = () => {
   return (
     <div className='flex flex-col items-center w-100'>
-      <a href='tel:+1-913-353-5802'>
+      <a href="tel:+1-913-353-5802">
         <div className='text-white font-title-text text-3xl my-7 opacity-90'>
           (913)-353-5802
         </div>
